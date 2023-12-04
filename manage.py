8d335e887acd38a671a60a51d923a3d23669dc83
bi0punk <systecinfo_cl@criptext.com>
2023-12-04 00:00:05 -0300

    clear

--- manage.py
+++ manage.py
@@ -10,6 +10,9 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            "No se pudo importar Django. ¿Estás seguro de que está instalado y"
+            "¿disponible en su variable de entorno PYTHONPATH? ¿Lo hizo?"
+            "¿Olvidaste activar un entorno virtual?"
         ) from exc
     execute_from_command_line(sys.argv)
 
